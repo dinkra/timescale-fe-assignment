a31c6c0d3341dba6589fc43ee44c018200b6f6ba
dinkra <dinarochka@gmail.com>
2022-12-06 20:41:48 +0100

    [feat]: add theme

---
+++ src/theme/typography.js
@@ -0,0 +1,14 @@
+// Font family
+export const FONT_FAMILY = '"Inter"';
+
+const typography = {
+  fontFamily: FONT_FAMILY,
+  fontWeightNormal: '400',
+  fontWeightBold: '700',
+
+  fontSizeSmall: `${12 / 16}rem`,
+  fontSizeNormal: `${14 / 16}rem`,
+  fontSizeLarge: `${18 / 16}rem`,
+};
+
+export default typography;
